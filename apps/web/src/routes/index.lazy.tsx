4fdcc8b97ac2f58fd6b5ad903c9a945f9306013a
Haziq Khairi <ikhairulhaziq11@gmail.com>
2024-08-10 01:17:30 +0800

    💄 style: change to `ImageUp` by lucide-react instead of custom svg

--- apps/web/src/routes/index.lazy.tsx
+++ apps/web/src/routes/index.lazy.tsx
@@ -2,7 +2,7 @@ import { useDropzone } from "react-dropzone";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { toast } from "sonner";
-import { Upload } from "lucide-react";
+import { ImageUp, Upload } from "lucide-react";
 
 import useStore from "@/stores/useStore";
 import { uploadFile, executeFile } from "@/api";
@@ -118,9 +118,7 @@ function Index() {
                       <p className="text-gray-700">{file.name}</p>
                     ) : (
                       <div>
-                        <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                          <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                        </svg>
+                        <ImageUp className="mx-auto h-12 w-12 text-gray-400" />
                         <div className="flex text-sm text-gray-600">
                           <label className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                             <span>Upload a file</span>
