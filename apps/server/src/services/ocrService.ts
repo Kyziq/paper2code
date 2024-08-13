63a48459a278c642a98f837965925ec563039f6d
Haziq Khairi <ikhairulhaziq11@gmail.com>
2024-08-13 18:42:20 +0800

    ✨ feat: integrate `@google-cloud/storage` (GCP Bucket) for OCR file handling and storage and add logs

--- apps/server/src/services/ocrService.ts
+++ apps/server/src/services/ocrService.ts
@@ -1,19 +1,43 @@
 import vision from "@google-cloud/vision";
-import { promises as fs } from "fs";
 import path from "path";
-import { uploadDir } from "../utils/fileSystem";
 import dotenv from "dotenv";
 import { logger } from "../utils/logger";
+import { Storage } from "@google-cloud/storage";
 
 dotenv.config({ path: path.resolve(__dirname, "../../../../.env") });
 
 const client = new vision.ImageAnnotatorClient({
   keyFilename: process.env.GCP_SERVICE_ACCOUNT_KEY_PATH,
 });
+const storage = new Storage({
+  keyFilename: process.env.GCP_SERVICE_ACCOUNT_KEY_PATH,
+});
 
-export const handleImage = async (filePath: string): Promise<string> => {
+const bucketName = process.env.GCP_STORAGE_BUCKET_NAME!;
+const storageClass = process.env.GCP_STORAGE_CLASS!;
+const location = process.env.GCP_STORAGE_LOCATION!;
+
+async function createBucketIfNotExists() {
   try {
-    const [result] = await client.documentTextDetection(filePath);
+    const [bucketExists] = await storage.bucket(bucketName).exists();
+    if (!bucketExists) {
+      const [bucket] = await storage.createBucket(bucketName, {
+        location,
+        [storageClass]: true,
+      });
+      logger.info(`Bucket ${bucket.name} created with ${storageClass} class in ${location}`);
+    } else {
+      logger.info(`Using bucket ${bucketName} that has been created previously`);
+    }
+  } catch (error) {
+    logger.error(`Error creating/checking bucket: ${error}`);
+    throw error;
+  }
+}
+
+export const handleImage = async (fileName: string): Promise<string> => {
+  try {
+    const [result] = await client.documentTextDetection(`gs://${bucketName}/${fileName}`);
     const fullTextAnnotation = result.fullTextAnnotation;
 
     if (!fullTextAnnotation || !fullTextAnnotation.text) {
@@ -36,20 +60,36 @@ export const handlePDF = async (filePath: string): Promise<string> => {
 };
 
 export const performOCR = async (file: File): Promise<string> => {
-  const arrayBuffer = await file.arrayBuffer();
-  const uint8Array = new Uint8Array(arrayBuffer);
-  const uploadedFilePath = path.resolve(uploadDir, file.name);
-  await fs.writeFile(uploadedFilePath, uint8Array);
-  logger.info(`File saved to: ${uploadedFilePath}`);
-
   try {
-    const text = file.type === "application/pdf" ? await handlePDF(uploadedFilePath) : await handleImage(uploadedFilePath);
-    await fs.unlink(uploadedFilePath);
-    logger.info(`File deleted: ${uploadedFilePath}`);
+    await createBucketIfNotExists();
+
+    const bucket = storage.bucket(bucketName);
+    const blob = bucket.file(file.name);
+
+    // Get the array buffer of the file
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    // Upload the file to Google Cloud Storage
+    await blob.save(buffer, {
+      contentType: file.type,
+    });
+    logger.info(`File uploaded to bucket: ${file.name}`);
+
+    // Perform OCR
+    const text = file.type === "application/pdf" ? await handlePDF(file.name) : await handleImage(file.name);
+
     return text;
   } catch (error) {
-    await fs.unlink(uploadedFilePath).catch(logger.error);
     logger.error(`An error occurred during OCR processing: ${error}`);
     throw error;
+  } finally {
+    // Ensure attempt to delete the file whether OCR succeeds or fails
+    try {
+      await storage.bucket(bucketName).file(file.name).delete();
+      logger.info(`File deleted from bucket}: ${file.name}`);
+    } catch (deleteError) {
+      logger.error(`Failed to delete file from bucket: ${deleteError}`);
+    }
   }
 };
