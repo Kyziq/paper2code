d24ae8150ff144340b0608eab8eab8e562db2b7d
Kyziq <ihaziqkhairi.dev@gmail.com>
2025-01-13 09:47:20 +0800

    feat(config): set base path for Vite configuration

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+	base: "/paper2code/",
 	plugins: [TanStackRouterVite(), react()],
 	resolve: {
 		alias: {
